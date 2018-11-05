ffb6e01667a610bd43ab0626105088532995a5ab
Nicole Essex <Essex@Nicole-Essexs-MacBook-Pro-2.local>
2018-11-05 14:29:18 -0700

    first success php and mysql

---
+++ read-one.php
@@ -0,0 +1,32 @@
+<?php
+ 
+// 1. database credentials
+$host = "localhost";
+$db_name = "hello_world_db";
+$username = "root";
+$password = "root";
+
+ // 2. connect to database
+$con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+ 
+// 3. prepare select query
+$query = "SELECT id, name, description, price FROM products WHERE id = ? LIMIT 1";
+$stmt = $con->prepare( $query );
+ 
+// 4. sample product ID
+$product_id=2;
+ 
+// 5. this is the first question mark in the query
+$stmt->bindParam(1, $product_id);
+ 
+// 6. execute our query
+$stmt->execute();
+ 
+// 7. store retrieved row to the 'row' variable
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+// 8. show data to user
+echo "<div>Name: " . $row['name'] . "</div>";
+echo "<div>Description: " . $row['description'] . "</div>";
+echo "<div>Price: $" . $row['price'] . "</div>";
+?>
